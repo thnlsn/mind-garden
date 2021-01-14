53fab74505d90982a6a40ce137a0949aac345f5b
Thomas Nelson <thnlsn@gmail.com>
2021-01-14 00:55:27 -0800

    finished basic styling for header and question component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './css/style.css';
 
 function App() {
-  return <div className='App'>Mind Garden</div>;
+  return (
+    <div className='dashboard'>
+      <header className='header'>
+        <h1 className='heading-primary'>Mind Garden</h1>
+        <div className='sub-heading'>
+          Take a moment to plant the seeds for success.
+          <a href='#' className='link'>
+            Learn more &rarr;
+          </a>
+        </div>
+      </header>
+      <main className='planner'>
+        <div className='question'>
+          <h4 className='heading-secondary'>I am grateful for...</h4>
+          <div class='question__input' contenteditable='true'></div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
