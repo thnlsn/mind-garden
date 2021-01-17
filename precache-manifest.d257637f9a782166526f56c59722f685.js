938436e3042e3edad16abdbe4e2a7c93e5ed3879
Thomas Nelson <thnlsn@gmail.com>
2021-01-16 22:23:54 -0800

    Updates

---
+++ precache-manifest.d257637f9a782166526f56c59722f685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06cc0acc2c3a8b6ef6d12b044be23ba",
+    "url": "/mind-garden/index.html"
+  },
+  {
+    "revision": "16432a23e18e743bf3fe",
+    "url": "/mind-garden/static/css/main.0bdecf4c.chunk.css"
+  },
+  {
+    "revision": "c7c26d8876760262a6d0",
+    "url": "/mind-garden/static/js/2.bae1d651.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mind-garden/static/js/2.bae1d651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16432a23e18e743bf3fe",
+    "url": "/mind-garden/static/js/main.ec844403.chunk.js"
+  },
+  {
+    "revision": "58c20bf31b1b9f1ee8c2",
+    "url": "/mind-garden/static/js/runtime-main.275fdfe0.js"
+  },
+  {
+    "revision": "942a0ec898a4a5cc2bac8f7478950b45",
+    "url": "/mind-garden/static/media/background.942a0ec8.png"
+  }
+]);
